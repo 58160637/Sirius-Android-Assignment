eaf509cb5735e36d6f02c56a5482f788dfb3aa0e
waramporn <waramporn.yangkijkarn@scbtechx.io>
2022-03-25 22:25:06 +0700

    [assignment] display city from json
    (cherry picked from commit 3eabab4384a599e95919c6c7793fe82305371971)

--- app/src/main/java/com/waramporn/androidasignment/view/activity/MainActivity.kt
+++ app/src/main/java/com/waramporn/androidasignment/view/activity/MainActivity.kt
@@ -1,24 +1,43 @@
 package com.waramporn.androidasignment.view.activity
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import butterknife.BindView
+import butterknife.ButterKnife
 import com.waramporn.androidasignment.R
+import com.waramporn.androidasignment.view.adapter.CityListAdapter
 import com.waramporn.presentation.model.City
+import com.waramporn.presentation.model.CityLocation
 import com.waramporn.presentation.presenter.Presenter
 import com.waramporn.presentation.views.Contractor
 
-class MainActivity : AppCompatActivity(), Contractor.View {
+
+class MainActivity : AppCompatActivity(), Contractor.View, CityListAdapter.OnClickListener {
+
+    @BindView(R.id.rv_city)
+    lateinit var rvCity: RecyclerView
 
     lateinit var presenter: Presenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        presenter = Presenter(this,this)
+        ButterKnife.bind(this)
+        presenter = Presenter(this, this)
         presenter.start()
     }
 
     override fun showCityList(cities: List<City>) {
+        rvCity.layoutManager = LinearLayoutManager(this)
+        val cityListAdapter = CityListAdapter(cities, this)
+        rvCity.adapter = cityListAdapter
 
+    }
+
+    override fun onClick(location: CityLocation) {
+        Log.d("location", "click")
     }
 }
--- app/src/main/java/com/waramporn/androidasignment/view/adapter/CityListAdapter.kt
+++ app/src/main/java/com/waramporn/androidasignment/view/adapter/CityListAdapter.kt
@@ -5,30 +5,61 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import butterknife.BindView
+import butterknife.ButterKnife
 import com.waramporn.androidasignment.R
 import com.waramporn.presentation.model.City
+import com.waramporn.presentation.model.CityLocation
 
 class CityListAdapter(
-    private val cities: List<City>
-) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private val cities: List<City>,
+    private val listener: OnClickListener
+) : RecyclerView.Adapter<CityListAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
             LayoutInflater.from(parent.context).inflate(R.layout.custom_city_list, parent, false)
         )
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(cities[position], listener)
     }
 
     override fun getItemCount(): Int {
         return cities.size
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val city = itemView.findViewById(R.id.tv_city) as TextView
-        val lat = itemView.findViewById(R.id.tv_lat) as TextView
-        val lon = itemView.findViewById(R.id.tv_lon) as TextView
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        @BindView(R.id.tv_city)
+        lateinit var tvCity: TextView
+
+        @BindView(R.id.tv_lat)
+        lateinit var tvLat: TextView
+
+        @BindView(R.id.tv_lon)
+        lateinit var tvLon: TextView
+
+        @BindView(R.id.tv_arrow)
+        lateinit var tvArrow: TextView
+
+        init {
+            ButterKnife.bind(this, itemView)
+        }
+
+        fun bind(
+            item: City,
+            onClickListener: OnClickListener
+        ) {
+            tvCity.text = item.name + ", " + item.country
+            tvLat.text = item.coord.lat.toString()
+            tvLon.text = item.coord.lon.toString()
+            tvArrow.setOnClickListener { onClickListener.onClick(item.coord) }
+        }
+    }
+
+    interface OnClickListener {
+        fun onClick(location: CityLocation)
     }
 }
